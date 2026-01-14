34277f5052dce1745339193aae91edae01a275ba
MOHAMMEDMUJAHED830 <md.mujahed830@gmail.com>
2026-01-14 18:55:16 +0530

    check commit

---
+++ pre_Day4/Tuples&Sets.py
@@ -0,0 +1,14 @@
+# Q1. Basic Tuple Operations
+# Problem: Create tuple (5, 10, 15, 10, 20) and print:
+
+# First element
+# Last element
+# Slice from index 1 to 3
+
+t = (5,10,15,10,20)
+print(t[0])
+print(t[-1])
+print(t[1:4])
+
+
+
